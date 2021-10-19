2ce202ae0477c4d81b6f7769103076c9bad4667f
kelvinchoi26 <kelvinchoi@naver.com>
2021-10-20 00:55:54 +0900

    xcode 프로젝트 생성

---
+++ WeDo/WeDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeDo
+//
+//  Created by 최형민 on 2021/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
