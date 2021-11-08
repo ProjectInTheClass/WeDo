4bdebe66ef5661a04a1bc2d4efcb53794f3d2b01
yys88699 <58525719+yys88699@users.noreply.github.com>
2021-11-08 15:43:53 +0900

    이전 작업 풀

---
+++ WeDo/WeDo/MyProfileViewController.swift
@@ -0,0 +1,111 @@
+//
+//  MyProfileViewController.swift
+//  WeDo
+//
+//  Created by 윤영서 on 2021/10/21.
+//
+
+import UIKit
+
+
+//extension UIImage { func resize(newWidth: CGFloat) -> UIImage {
+//    let scale = newWidth / self.size.width
+//    let newHeight = self.size.height * scale
+//    let size = CGSize(width: newWidth, height: newHeight)
+//    let render = UIGraphicsImageRenderer(size: size)
+//    let renderImage = render.image { context in self.draw(in: CGRect(origin: .zero, size: size)) }
+//    return renderImage
+//    }
+//}
+    
+
+class MyProfileViewController: UIViewController {
+
+    var myName: String?
+    var myImage: UIImage?
+    var myEmail: String?
+    var myContact: String?
+    var myProfileMessage: String?
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var profileEmailLabel: UILabel!
+    @IBOutlet weak var profileContactLabel: UILabel!
+    @IBOutlet weak var profileMessageLabel: UILabel!
+    
+    @IBOutlet weak var emailEditBtn: UIButton!
+    @IBOutlet weak var contactEditBtn: UIButton!
+    @IBOutlet weak var messageEditBtn: UIButton!
+    @IBOutlet weak var emailEditConfirmBtn: UIButton!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        contactEditBtn.isHidden = true
+        messageEditBtn.isHidden = true
+        emailTextField.isHidden = true
+        emailEditBtn.isHidden = true
+        emailEditConfirmBtn.isHidden = true
+        emailTextField.isHidden = true
+
+        
+        
+        
+        
+        profileImageView.image = myImage
+        profileNameLabel.text = myName
+        profileEmailLabel.text = myEmail
+        profileContactLabel.text = myContact
+        profileMessageLabel.text = myProfileMessage
+    }
+    
+    
+    @IBAction func editButtonPressed(_ sender: Any) {
+        emailEditBtn.isHidden = false
+        contactEditBtn.isHidden = false
+        messageEditBtn.isHidden = false
+    }
+    
+    @IBAction func editEmailButtonPressed(_ sender: Any) {
+        emailTextField.isHidden = false
+        emailEditBtn.isHidden = true
+        profileEmailLabel.isHidden = true
+        emailEditConfirmBtn.isHidden = false
+        emailTextField.text = "\(myEmail!)"
+    }
+    
+    
+    
+    @IBAction func emailEditConfirmBtnPressed(_ sender: Any) {
+        
+        myEmail = emailTextField.text
+        profileEmailLabel.text = "\(myEmail!)"
+        emailEditConfirmBtn.isHidden = true
+        emailEditBtn.isHidden = false
+        emailTextField.isHidden = true
+        profileEmailLabel.isHidden = false
+    }
+    
+
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
+
+
