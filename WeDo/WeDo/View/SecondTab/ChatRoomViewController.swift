c43d81262b73385835129e800cbf43fe9fcec278
yys88699 <58525719+yys88699@users.noreply.github.com>
2021-11-13 04:11:05 +0900

    채팅방화면 -  textfield 클릭시 키보드 같이 움직이도록 설정

--- WeDo/WeDo/View/SecondTab/ChatRoomViewController.swift
+++ WeDo/WeDo/View/SecondTab/ChatRoomViewController.swift
@@ -7,21 +7,57 @@
 
 import UIKit
 
-class ChatRoomViewController: UIViewController {
 
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self,action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+        view.frame.origin.y = 0
+
+    }
+}
+
+class ChatRoomViewController: UIViewController, UITextFieldDelegate {
+
+    
     @IBOutlet weak var plusBtn: UIButton!
     @IBOutlet weak var todoBtn: UIButton!
     @IBOutlet weak var checklistBtn: UIButton!
+    @IBOutlet weak var myTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.myTextField.delegate = self
+
         
         todoBtn.isHidden = true
         checklistBtn.isHidden = true
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
 
-        // Do any additional setup after loading the view.
+        self.hideKeyboardWhenTappedAround()
     }
     
+    
+//    이 부분과 아래 부분이 안 먹히는 것 같음 => 임시로 다른 함수 추가해 리턴시 돌아가게 했지만 다른해결 필요
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        view.frame.origin.y = 0
+        return false
+    }
+
     
     var moveBtnPressed: Bool = false
     @IBAction func plusBtnTapped(_ sender: UIButton) {
@@ -53,28 +89,25 @@ class ChatRoomViewController: UIViewController {
                     self.checklistBtn.isHidden = true
                 }
             }
-
-
-//            let group = DispatchGroup()
-//
-//            group.enter()
-//            UIView.animate(withDuration: 0.5, animations: {
-//                self.todoBtn.frame.origin.y += 50
-//                self.checklistBtn.frame.origin.y += 100
-//            }, completion: nil)
-//
-//            group.leave()
-//            group.notify(queue: .main){
-//                self.todoBtn.isHidden = true
-//                self.checklistBtn.isHidden = true
-//            }
         }
-        
-        
     }
     
-    
-    
+    @objc func keyboardWillChange(notification: Notification) {
+
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+
+
+        // 안되는 부분 유추
+        if notification.name == UIResponder.keyboardWillShowNotification ||
+            notification.name == UIResponder.keyboardWillHideNotification {
+
+            view.frame.origin.y = -(keyboardRect.height - 70)
+        } else {
+            view.frame.origin.y = 0
+        }
+    }
     
 }
     
